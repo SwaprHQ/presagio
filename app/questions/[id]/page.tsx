9e47fdbc98c87e91285557c60508e62589fbdba9
Berteotti <berteotti2@gmail.com>
2024-04-03 11:13:27 +0100

    feat: add graphl and omen queries

--- app/questions/[id]/page.tsx
+++ app/questions/[id]/page.tsx
@@ -1,23 +1,38 @@
 import { SwapboxContainer } from "@/app/components";
+import { QueryClient } from "@tanstack/react-query";
+import {
+  FixedProductMarketMaker_OrderBy,
+  OrderDirection,
+  getMarkets,
+} from "@/queries/omen";
 
 // Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
-  // will do the fetching here
+  const queryClient = new QueryClient();
+  const markets = await queryClient.fetchQuery({
+    queryKey: ["getMarkets"],
+    queryFn: async () =>
+      getMarkets({
+        first: 10,
+        skip: 0,
+        orderBy: FixedProductMarketMaker_OrderBy.CreationTimestamp,
+        orderDirection: OrderDirection.Desc,
+      }),
+  });
 
-  const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
-  return ids.map(id => ({
-    id: id.toString(),
+  return markets.fixedProductMarketMakers.map(({ id }) => ({
+    id,
   }));
 }
 
-export default function QuestionPage({ params }: { params: { id: string } }) {
-  const { id } = params;
-  console.log("id:", id);
-
+export default async function QuestionPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   return (
     <main className="w-full px-6 mt-12 space-y-12 md:flex md:flex-col md:items-center">
-      <SwapboxContainer />
+      <SwapboxContainer id={params.id} />
     </main>
   );
 }
