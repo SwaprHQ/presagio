9f24d1c5d30e0be8023d5eceab2ac7658b3b4e34
Diogo Ferreira <diogomf@hey.com>
2024-04-08 15:59:42 +0100

    - Add CardBet (#8)
    - Add loading with LoadingCardBet
    - data is just for mocking

---
+++ app/components/CardBet.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { Card } from "@/app/components/ui";
+import { FixedProductMarketMaker } from "@/queries/omen";
+import { remainingTime } from "@/utils/dates";
+import { cx } from "class-variance-authority";
+import Link from "next/link";
+import { Button, Logo, Tag } from "swapr-ui";
+
+interface CardBetProps {
+  market: FixedProductMarketMaker;
+}
+
+export const CardBet = ({ market }: CardBetProps) => {
+  const closingDate = new Date(+market.openingTimestamp * 1000);
+
+  const outcomes = ["win", "loss", "-"];
+  const randomOutcome = outcomes[Math.floor(Math.random() * outcomes.length)];
+
+  return (
+    <Card
+      className={cx(
+        "w-full bg-gradient-to-b from-[#131313]",
+        randomOutcome === "win" && "from-[#131313] to-[#11301F]",
+        randomOutcome === "loss" && "from-[#131313] to-[#301111]"
+      )}
+    >
+      <Link key={market.id} href={`markets/${market.id}`} className="block">
+        <section className="p-4 h-[144px] flex flex-col justify-between space-y-4">
+          <div className="flex justify-between items-center">
+            <div className="flex space-x-2">
+              <Tag colorScheme="quaternary" size="sm">
+                Election
+              </Tag>
+              <Tag colorScheme="success" size="sm">
+                You chose Yes
+              </Tag>
+            </div>
+            <p className="text-sm text-text-low-em">
+              {remainingTime(closingDate)}
+            </p>
+          </div>
+          <div className="flex space-x-4 ">
+            <div className="size-[40px] bg-text-low-em rounded-8 bg-gradient-to-r from-[#cb8fc1] to-[#b459c6]" />
+            <div className="flex-1 text-normal md:text-xl font-semibold text-text-high-em h-[80px] overflow-y-auto">
+              {market.title}
+            </div>
+          </div>
+        </section>
+      </Link>
+      <section className="flex items-center h-[56px] md:h-[48px] px-4 border-t border-outline-base-em">
+        <div className="flex items-center justify-between w-full space-x-4">
+          <div className="flex flex-col items-start md:flex-row md:items-center md:space-x-2 space-y-0.5">
+            <div className="flex items-center space-x-1">
+              <p className="text-sm font-semibold text-text-med-em">
+                Bet amount:
+              </p>
+              <p className="text-sm font-semibold text-text-high-em">
+                200 <span>SDAI</span>
+              </p>
+              <Logo
+                src="https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/tokens/100/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/logo-128.png"
+                alt="token logo"
+                size="xs"
+              />
+            </div>
+            <div className="flex items-center space-x-1">
+              <p className="text-sm font-semibold text-text-med-em">You won:</p>
+              <p className="text-sm font-semibold text-text-high-em">
+                300 <span>SDAI</span>
+              </p>
+              <Logo
+                src="https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/tokens/100/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/logo-128.png"
+                alt="token logo"
+                size="xs"
+              />
+            </div>
+          </div>
+          <Button size="sm" colorScheme="success" variant="pastel">
+            Reedem
+          </Button>
+        </div>
+      </section>
+    </Card>
+  );
+};
+
+export const LoadingCardBet = () => (
+  <Card className="md:w-[760px] h-[194px] p-4 flex flex-col justify-between">
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <div className="w-48 h-8 rounded-8 bg-outline-low-em animate-pulse"></div>
+        <div className="w-32 h-8 rounded-8 bg-outline-low-em animate-pulse"></div>
+      </div>{" "}
+      <div className="h-24 rounded-8 bg-outline-low-em animate-pulse"></div>
+    </div>
+    <div className="flex items-center justify-between">
+      <div className="w-48 h-8 rounded-8 bg-outline-low-em animate-pulse"></div>
+      <div className="w-20 h-8 rounded-8 bg-outline-low-em animate-pulse"></div>
+    </div>
+  </Card>
+);
--- app/my-bets/page.tsx
+++ app/my-bets/page.tsx
@@ -1,17 +1,76 @@
-import { RowCard } from "@/app/components/ui";
-import Link from "next/link";
+"use client";
+
+import { CardBet, LoadingCardBet } from "@/app/components/CardBet";
+import {
+  FixedProductMarketMaker_OrderBy,
+  OrderDirection,
+  getMarkets,
+} from "@/queries/omen";
+import { useQuery } from "@tanstack/react-query";
+import { TabBody, TabGroup, TabHeader, TabPanel, TabStyled } from "swapr-ui";
 
 export default function MyBetsPage() {
+  const { data: markets, isLoading } = useQuery({
+    queryKey: ["getMarkets"],
+    queryFn: async () =>
+      getMarkets({
+        first: 10,
+        skip: 0,
+        orderBy: FixedProductMarketMaker_OrderBy.CreationTimestamp,
+        orderDirection: OrderDirection.Desc,
+      }),
+  });
+
   return (
     <div className="w-full px-6 mt-12 space-y-12 md:items-center md:flex md:flex-col">
       <div>
-        <h1 className="mb-12 text-2xl font-semibold text-white">My bets</h1>
-        <div className="space-y-5 w-full md:w-[760px]">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map(q => (
-            <Link key={q} href={`markets/${q.toString()}`} className="block">
-              <RowCard />
-            </Link>
-          ))}
+        <h1 className="mb-8 text-2xl font-semibold text-white">My bets</h1>
+        <div className="md:w-[760px]">
+          <TabGroup
+            onChange={(index: number) =>
+              console.log("Changed selected tab to:", index)
+            }
+          >
+            <TabHeader className="overflow-x-auto md:overflow-x-visible">
+              <TabStyled>
+                All bets{" "}
+                <div className="bg-surface-surface-0 text-2xs border border-outline-low-em rounded-6 p-1 px-1.5 ml-2">
+                  09
+                </div>
+              </TabStyled>
+              <TabStyled>Active</TabStyled>
+              <TabStyled>Unredeemed</TabStyled>
+              <TabStyled>Complete</TabStyled>
+            </TabHeader>
+
+            <TabBody className="mt-8">
+              <TabPanel className="space-y-4">
+                {isLoading
+                  ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(index => (
+                      <LoadingCardBet key={index} />
+                    ))
+                  : markets?.fixedProductMarketMakers &&
+                    markets.fixedProductMarketMakers.map(market => (
+                      <CardBet market={market} key={market.id} />
+                    ))}
+              </TabPanel>
+              <TabPanel>
+                <div className="bg-surface-primary-accent-1 p-5 rounded-4">
+                  in development..
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="bg-surface-danger-accent-1 p-5 rounded-4">
+                  in development..
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="bg-surface-warning-accent-1 p-5 rounded-4">
+                  Come back in a week!
+                </div>
+              </TabPanel>
+            </TabBody>
+          </TabGroup>
         </div>
       </div>
     </div>
