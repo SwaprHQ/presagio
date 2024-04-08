3b6aeea0d9a9a2e00fc8a511a36dbc7ce6049331
Berteotti <berteotti2@gmail.com>
2024-04-08 17:33:16 +0100

    feat: create swapbox input

--- app/components/Swapbox.tsx
+++ app/components/Swapbox.tsx
@@ -1,1 +1,45 @@
-export const Swapbox = () => <h1 className="text-2xl font-bold">Swapbox</h1>;
+"use client";
+
+import { Button, IconButton } from "swapr-ui";
+import { SwapInput } from "./ui/SwapInput";
+import { useState } from "react";
+
+export const Swapbox = () => {
+  const [tokenInAmount, setTokenInAmount] = useState("");
+  const [tokenOutAmount, setTokenOutAmount] = useState("");
+
+  return (
+    <div className="space-y-2 relative">
+      <SwapInput
+        title="You Swap"
+        value={tokenInAmount}
+        onChange={(event) => {
+          setTokenInAmount(event.target.value);
+        }}
+        onClick={() => console.log("I want to change token")}
+      >
+        <div className="flex text-sm items-center justify-end space-x-1.5">
+          <p className="text-text-low-em">Balance: 5400</p>
+          <Button
+            variant="ghost"
+            className="text-sm font-semibold text-text-primary-main"
+          >
+            Use MAX
+          </Button>
+        </div>
+      </SwapInput>
+      <IconButton
+        name="swap-vertical"
+        variant="outline"
+        className="absolute top-[100px] left-[calc(50%_-_20px)]"
+      />
+      <SwapInput
+        title="To Receive"
+        value={tokenOutAmount}
+        onChange={(event) => {
+          setTokenOutAmount(event.target.value);
+        }}
+      />
+    </div>
+  );
+};
---
+++ app/components/ui/SwapInput.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  ChangeEventHandler,
+  MouseEventHandler,
+  PropsWithChildren,
+} from "react";
+import { Button, Icon, Logo } from "swapr-ui";
+
+interface SwapInputProps extends PropsWithChildren {
+  title: string;
+  value?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const SwapInput = ({
+  title,
+  children,
+  value,
+  onChange,
+  onClick,
+}: SwapInputProps) => {
+  return (
+    <div className="p-4 space-y-1 bg-surface-surface-1 rounded-16">
+      <div className="text-xs uppercase text-text-low-em font-semibold">
+        {title}
+      </div>
+      <div className="flex w-full space-x-2">
+        <input
+          min={0}
+          type="number"
+          pattern="[0-9]*"
+          placeholder="0.0"
+          value={value}
+          onWheel={(event) => event.currentTarget.blur()}
+          onKeyDown={(evt) =>
+            ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()
+          }
+          onChange={onChange}
+          className="bg-transparent overflow-hidden overscroll-none outline-none caret-text-primary-main text-3xl placeholder:text-text-disabled"
+        />
+        <Button variant="pastel" onClick={onClick}>
+          <Logo
+            src="https://raw.githubusercontent.com/SmolDapp/tokenAssets/main/tokens/100/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/logo-128.png"
+            alt="token logo"
+            size="xs"
+          />
+          <p className="font-semibold">SDAI</p>
+          <Icon name="chevron-down" />
+        </Button>
+      </div>
+      {children}
+    </div>
+  );
+};
