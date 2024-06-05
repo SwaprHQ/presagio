bf91a7281ef39c805a325d8a176f46ef10bc1e04
Diogo Ferreira <diogomf@hey.com>
2024-06-05 11:58:50 +0100

    Refactor code to handle writing contract and showing transaction modal - create Tx Context

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -6,8 +6,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { config } from "./config";
 import { ConnectKitProvider } from "connectkit";
-import { ModalContextProvider } from "@/context/ModalContext";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { TxProvider, ModalContextProvider } from "@/context";
 
 const queryClient = new QueryClient();
 
@@ -21,7 +21,9 @@ export const Providers = ({ children }: PropsWithChildren) => {
       <QueryClientProvider client={queryClient}>
         <ConnectKitProvider options={connectKitOptions}>
           <ModalContextProvider>
-            <NextThemesProvider>{children}</NextThemesProvider>
+            <TxProvider>
+              <NextThemesProvider>{children}</NextThemesProvider>
+            </TxProvider>
           </ModalContextProvider>
         </ConnectKitProvider>
       </QueryClientProvider>
