2807c0ad7c7d2ab51d8fe29213373bbfb3c5d4f5
Berteotti <berteotti2@gmail.com>
2024-08-19 17:33:16 +0100

    feat: show market resolution status

--- utils/token.ts
+++ utils/token.ts
@@ -1,5 +1,7 @@
+import { GNOSIS_SCAN_URL } from '@/constants';
+
 export const shortenAddress = (address: string) =>
   `${address.slice(0, 6)}...${address.slice(-4)}`;
 
 export const getGnosisAddressExplorerLink = (address: string) =>
-  `https://gnosisscan.io/address/${address}`;
+  `${GNOSIS_SCAN_URL}/address/${address}`;
