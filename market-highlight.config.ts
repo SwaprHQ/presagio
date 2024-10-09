4c7880b30c50c6216e3900e110465fedec4270d0
Berteotti <berteotti2@gmail.com>
2024-10-09 15:40:40 +0700

    remove markets to highlight

--- market-highlight.config.ts
+++ market-highlight.config.ts
@@ -5,14 +5,8 @@
 //
 // '0x0539590c0cf0d929e3f40b290fda04b9b4a8cf68': { image: kamalaTrump }
 
-import kamalaTrump from '@/public/assets/highlights/kamala-trump.png';
 import { StaticImageData } from 'next/image';
 
-const highlightedMarketsList: Record<string, { image: StaticImageData }> = {
-  '0x8943a769cb0503f7bc4e92a777fcd1aa9decfc33': {
-    image: kamalaTrump,
-  },
-  '0x0539590c0cf0d929e3f40b290fda04b9b4a8cf68': { image: kamalaTrump },
-};
+const highlightedMarketsList: Record<string, { image: StaticImageData }> = {};
 
 export { highlightedMarketsList };
