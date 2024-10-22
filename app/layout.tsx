574f8fb77f08ccb1f4b2972bc04b4e67096b6a02
Diogo Ferreira <diogomf@hey.com>
2024-10-23 00:41:55 +0900

    - add twitter and open-graph meta images file - update meta description and title - add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,15 @@ import { Manrope } from 'next/font/google';
 import './globals.css';
 import '@swapr/ui/colors.css';
 
-import { APP_NAME } from '@/constants';
+import { APP_NAME, APP_URL } from '@/constants';
 import { Suspense } from 'react';
 
 const manrope = Manrope({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: `${APP_NAME} 👁️ - The Prediction Market on Gnosis with AI`,
-  description: 'Bet on your predicitons. Analyse AI bets.',
+  metadataBase: new URL(APP_URL),
+  title: `${APP_NAME} - Where predictions meet AI agents`,
+  description: 'Bet now on multiple markets in gnosis chain.',
 };
 
 export default function RootLayout({
