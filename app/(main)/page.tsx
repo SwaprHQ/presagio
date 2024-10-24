e0a968da569469500752f6b05b384a2cd73fefa6
ElRodrigote <lil.rorry@gmail.com>
2024-10-24 13:02:28 +0200

    feat: add events tracking for our landing and markets list display interactions

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -31,8 +31,11 @@ import {
 } from './filters';
 import { isAddress } from 'viem';
 import Image from 'next/image';
+import { trackEvent } from 'fathom-client';
+
 import { getOpenMarkets } from '@/queries/dune';
-import { Categories } from '@/constants';
+import { Categories, FA_EVENTS } from '@/constants';
+
 import { MarketsHighlight } from './MarketsHighlight';
 import { DevconflictBanner } from '@/app/components/Devconflict';
 
@@ -311,6 +314,9 @@ export default function HomePage() {
                   value={categoryOption}
                   className="font-semibold capitalize"
                   size="md"
+                  onClick={() =>
+                    trackEvent(FA_EVENTS.MARKETS.CATEGORY(categoryOption as string))
+                  }
                 >
                   {categoryOption}
                 </ToggleGroupOption>
@@ -325,6 +331,7 @@ export default function HomePage() {
           placeholder="Search markets keywords or address"
           leftIcon="search"
           onChange={event => handleSearch(event.target.value)}
+          onClick={() => trackEvent(FA_EVENTS.MARKETS.SEARCH_BAR)}
           value={search}
         />
         {showClientUI ? (
@@ -342,7 +349,10 @@ export default function HomePage() {
               {creatorFilters.map(option => (
                 <div
                   key={option.key}
-                  onClick={() => selectCreatorFilter(option)}
+                  onClick={() => {
+                    selectCreatorFilter(option);
+                    trackEvent(FA_EVENTS.MARKETS.FILTERS.CREATOR(option.name));
+                  }}
                   className="flex cursor-pointer items-center justify-start space-x-2 px-3 py-2 font-semibold"
                 >
                   <Icon
@@ -382,7 +392,10 @@ export default function HomePage() {
               {tokenFilters.map(option => (
                 <div
                   key={option.key}
-                  onClick={() => selectTokenFilter(option)}
+                  onClick={() => {
+                    selectTokenFilter(option);
+                    trackEvent(FA_EVENTS.MARKETS.FILTERS.TOKEN(option.name));
+                  }}
                   className="flex cursor-pointer items-center justify-start space-x-2 px-3 py-2 font-semibold"
                 >
                   <Icon
@@ -417,7 +430,10 @@ export default function HomePage() {
               {orderFilters.map(option => (
                 <div
                   key={option.key}
-                  onClick={() => selectOrderFilter(option)}
+                  onClick={() => {
+                    selectOrderFilter(option);
+                    trackEvent(FA_EVENTS.MARKETS.FILTERS.ORDER(option.name));
+                  }}
                   className="flex cursor-pointer items-center justify-start space-x-2 px-3 py-2 font-semibold"
                 >
                   <Icon
@@ -453,7 +469,10 @@ export default function HomePage() {
               {stateFilters.map(option => (
                 <div
                   key={option.key}
-                  onClick={() => selectStateFilter(option)}
+                  onClick={() => {
+                    selectStateFilter(option);
+                    trackEvent(FA_EVENTS.MARKETS.FILTERS.STATE(option.name));
+                  }}
                   className="flex cursor-pointer items-center justify-start space-x-2 px-3 py-2 font-semibold"
                 >
                   <Icon
@@ -483,7 +502,11 @@ export default function HomePage() {
       ) : markets?.length ? (
         <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
           {markets.map(market => (
-            <Link key={market.id} href={`markets?id=${market.id}`}>
+            <Link
+              key={market.id}
+              href={`markets?id=${market.id}`}
+              onClick={() => trackEvent(FA_EVENTS.MARKETS.DETAILS.ID(market.id))}
+            >
               <CardMarket market={market} />
             </Link>
           ))}
