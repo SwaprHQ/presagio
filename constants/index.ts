ef7b241f75d1a0d0ad816783210e93fc200919d5
ElRodrigote <lil.rorry@gmail.com>
2024-10-25 11:20:47 +0200

    feat: add footer and embed market events, update market list and market details event labels

--- constants/index.ts
+++ constants/index.ts
@@ -80,33 +80,53 @@ export const GITHUB_URL =
 const formatEventName = (eventName: string) =>
   eventName.split(' ').join('-').toLocaleLowerCase();
 export const FA_EVENTS = {
+  FOOTER: {
+    CONNECT: {
+      DISCORD: 'click/footer-connect_discord',
+      GITHUB: 'click/footer-connect_github',
+      X: 'click/footer-connect_x',
+    },
+    HOME: 'click/footer-home',
+    MARKET_CATEGORY: (category: string) => `click/footer-markets_${category}`,
+    POWERED_BY: {
+      GNOSIS: 'click/footer-powered-by_gnosis',
+      GNOSIS_AI: 'click/footer-powered-by_gnosis-ai',
+      KLEROS: 'click/footer-powered-by_kleros',
+      REALITY: 'click/footer-powered-by_reality',
+    },
+  },
   GET_SDAI: `click/get-${SDAI.symbol}`,
   MARKETS: {
-    CATEGORY: (category: string) => `click/market-category_${category}`,
+    CATEGORY: (category: string) => `click/markets_category-${category}`,
     DETAILS: {
+      EMBED: {
+        COPIED: (id: string) => `click/market-details-embed_copied-${id}`,
+        ID: (id: string) => `click/market-details-embed_${id}`,
+      },
       ID: (id: string) => `click/market-details_${id}`,
       INFO: {
-        CONTRACT: (contract: string) => `click/market-details-info_external-${contract}`,
+        CONTRACT: (contract: string) =>
+          `click/market-details-info_chain-scan-${contract}`,
         DISPUTE: (contract: string) => `click/market-details-info_kleros-${contract}`,
         ORACLE_ANSWER: (realityId: string) =>
           `click/market-details-info_reality-${realityId}`,
       },
       TABS: {
         NAME: (id: string, tabName: string) =>
-          `click/market-details-tab_${tabName}-${id}`,
-        NEWS_ARTICLE: (id: string) => `click/market-details-tab_news-article-${id}`,
+          `click/market-details-tabs_${tabName}-${id}`,
+        NEWS_ARTICLE: (id: string) => `click/market-details-tabs_news-article-${id}`,
       },
     },
     FILTERS: {
       CREATOR: (creatorName: string) =>
-        `click/market-filter-creator_${formatEventName(creatorName)}`,
+        `click/markets-filters_creator-${formatEventName(creatorName)}`,
       ORDER: (orderName: string) =>
-        `click/market-filter-order_${formatEventName(orderName)}`,
+        `click/markets-filters_order-${formatEventName(orderName)}`,
       STATE: (stateName: string) =>
-        `click/market-filter-state_${formatEventName(stateName)}`,
+        `click/markets-filters_state-${formatEventName(stateName)}`,
       TOKEN: (tokenName: string) =>
-        `click/market-filter-token_${formatEventName(tokenName)}`,
+        `click/markets-filters_token-${formatEventName(tokenName)}`,
     },
-    SEARCH_BAR: `click/market-search-bar`,
+    SEARCH_BAR: `click/markets_search-bar`,
   },
 };
