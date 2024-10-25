d6a96bc76668b38d64e7200ffae8cf901abd444c
ElRodrigote <lil.rorry@gmail.com>
2024-10-25 16:19:08 +0200

    feat: update FA event labels hierarchies

--- analytics/index.ts
+++ analytics/index.ts
@@ -5,58 +5,58 @@ const formatEventName = (eventName: string) =>
 
 export const FA_EVENTS = {
   BETS: {
-    MY_BETS: 'click/bets_my-bets',
-    REDEEM: 'click/bets_redeem',
+    MY_BETS: 'click/bets/my-bets',
+    REDEEM: 'click/bets/redeem',
   },
   DEVCONFLICT: 'click/devconflict-banner',
   FOOTER: {
     CONNECT: {
-      DISCORD: 'click/footer-connect_discord',
-      GITHUB: 'click/footer-connect_github',
-      X: 'click/footer-connect_x',
+      DISCORD: 'click/footer/connect/discord',
+      GITHUB: 'click/footer/connect/github',
+      X: 'click/footer/connect/x',
     },
-    HOME: 'click/footer-home',
-    MARKET_CATEGORY: (category: string) => `click/footer-markets_${category}`,
+    HOME: 'click/footer/home',
+    MARKET_CATEGORY: (category: string) => `click/footer/markets/${category}`,
     POWERED_BY: {
-      GNOSIS: 'click/footer-powered-by_gnosis',
-      GNOSIS_AI: 'click/footer-powered-by_gnosis-ai',
-      KLEROS: 'click/footer-powered-by_kleros',
-      REALITY: 'click/footer-powered-by_reality',
+      GNOSIS: 'click/footer/powered-by/gnosis',
+      GNOSIS_AI: 'click/footer/powered-by/gnosis-ai',
+      KLEROS: 'click/footer/powered-by/kleros',
+      REALITY: 'click/footer/powered-by/reality',
     },
   },
   LIFI_WIDGET: {
-    OPEN: `click/lifi-widget_open-${SDAI.symbol}`,
-    ROUTE_FAILED: 'click/lifi-widget_route-failed',
-    ROUTE_SUCCESS: 'click/lifi-widget_route-success',
+    OPEN: `click/lifi-widget/open-${SDAI.symbol}`,
+    ROUTE_FAILED: 'click/lifi-widget/route-failed',
+    ROUTE_SUCCESS: 'click/lifi-widget/route-success',
   },
   MARKETS: {
-    CATEGORY: (category: string) => `click/markets_category-${category}`,
+    CATEGORY: (category: string) => `click/markets/category-${category}`,
     DETAILS: {
       EMBED: {
-        COPIED: 'click/market-details-embed_copied',
-        ID: 'click/market-details-embed',
+        COPIED: 'click/markets/details/embed/copied',
+        ID: 'click/markets/details/embed',
       },
-      ID: (id: string) => `click/market-details_${id}`,
+      ID: (id: string) => `click/markets/details/${id}`,
       INFO: {
-        CONTRACT: 'click/market-details-info_chain-scanner',
-        DISPUTE: 'click/market-details-info_kleros',
-        ORACLE_ANSWER: 'click/market-details-info_reality',
+        CONTRACT: 'click/markets/details/info/chain-scanner',
+        DISPUTE: 'click/markets/details/info/kleros',
+        ORACLE_ANSWER: 'click/markets/details/info/reality',
       },
       TABS: {
-        NAME: (tabName: string) => `click/market-details-tabs_${tabName}`,
-        NEWS_ARTICLE: 'click/market-details-tabs_news-article',
+        NAME: (tabName: string) => `click/markets/details/tabs/${tabName}`,
+        NEWS_ARTICLE: 'click/markets/details/tabs/news-article',
       },
     },
     FILTERS: {
       CREATOR: (creatorName: string) =>
-        `click/markets-filters_creator-${formatEventName(creatorName)}`,
+        `click/markets/filters/creator-${formatEventName(creatorName)}`,
       ORDER: (orderName: string) =>
-        `click/markets-filters_order-${formatEventName(orderName)}`,
+        `click/markets/filters/order-${formatEventName(orderName)}`,
       STATE: (stateName: string) =>
-        `click/markets-filters_state-${formatEventName(stateName)}`,
+        `click/markets/filters/state-${formatEventName(stateName)}`,
       TOKEN: (tokenName: string) =>
-        `click/markets-filters_token-${formatEventName(tokenName)}`,
+        `click/markets/filters/token-${formatEventName(tokenName)}`,
     },
-    SEARCH_BAR: `click/markets_search-bar`,
+    SEARCH_BAR: `click/markets/search-bar`,
   },
 };
