99c42d1850ce0abcb95a80c9cf3d84ce0a9034c3
Diogo Ferreira <diogomf@hey.com>
2025-01-08 20:41:56 +0800

    make navbar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import { useWidgetEvents, WidgetEvent } from '@lifi/widget';
 import Link from 'next/link';
 import { FA_EVENTS } from '@/analytics';
-import { Button } from '@swapr/ui';
+import { Button, Icon } from '@swapr/ui';
 import { ConnectButton, SettingsPopover, LifiWidgetPopover } from '@/app/components';
 import { APP_NAME } from '@/constants';
 import { useQuery } from '@tanstack/react-query';
@@ -15,7 +15,6 @@ import { useMemo } from 'react';
 
 import { NetworkButton } from './NetworkButton';
 import { trackEvent } from 'fathom-client';
-import Image from 'next/image';
 import BetsIcon from '@/app/components/ui/icons/BetsIcon';
 
 export const Navbar = () => {
@@ -60,8 +59,9 @@ export const Navbar = () => {
             <LifiWidgetPopover />
           </div>
           <Link href="/leaderboard">
-            <Button variant="pastel" className="space-x-1 text-nowrap">
-              AI Leaderboard
+            <Button variant="pastel" className="space-x-2 text-nowrap">
+              <Icon name="bar-graph-fill" className="text-[16px] text-text-med-em" />
+              <p className="hidden md:block">Agents</p>
             </Button>
           </Link>
           <Link href="/my-bets">
@@ -72,7 +72,7 @@ export const Navbar = () => {
                 onClick={() => trackEvent(FA_EVENTS.BETS.MY_BETS)}
               >
                 <BetsIcon className="text-text-med-em" width={16} height={16} />
-                <p> My bets</p>
+                <p className="hidden md:block"> My bets</p>
               </Button>
               {hasUnredeemedBets && (
                 <div className="absolute -right-2 -top-1 flex size-5 items-center justify-center rounded-100 border-2 border-surface-surface-bg bg-surface-success-main p-1 text-text-white">
