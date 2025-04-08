62177fd380ce1d55f89bf957bc18b47c13e21d24
Diogo Ferreira <diogomf@hey.com>
2025-04-08 13:26:03 +0100

    fix svg

--- app/components/ui/icons/Sidebar.tsx
+++ app/components/ui/icons/Sidebar.tsx
@@ -13,9 +13,9 @@ const SidebarIcon = ({ className, width = 24, height = 24 }: SidebarIconProps) =
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}
     >
       <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
